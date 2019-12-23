36c50549a65a09e9b5d61b4b9979e2b3f9b537a3
Martin Moy <martinmoy1991@gmail.com>
2019-12-23 23:30:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,16 +2,14 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
-  @@genres = []
-  @@artists = []
+  
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count +=1
-    @@genres << genre
-    @@artists << artist
+  
   end
 
   def self.count

8b2838bcc32a8786ed3dc2afaa3df26d02c747d9
Martin Moy <martinmoy1991@gmail.com>
2019-12-23 23:29:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,24 +18,6 @@ class Song
     @@count
   end
 
-  def self.genres
-    @@genres.uniq
-  end
-
-  def self.artists
-    @@artists.uniq
-  end
-
-  def self.artist_count
-   @@artists.each_with_object(Hash.new(0)) do |key, hash|
-     hash[key] += 1
-   end
- end
-
- def self.genre_count
-    @@genres.each_with_object(Hash.new(0)) do |key, hash|
-      hash[key] += 1
-    end
-  end
+  
 
 end
